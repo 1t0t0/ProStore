8fb97bb87553977806be5ea1ec7e7859a37a2734
1t0t0 <totophandolack915@gmail.com>
2025-04-09 11:18:49 +0700

    add product details page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -13,3 +13,10 @@ export async function getLatestProducts() {
   });
   return convertToPlainObject(data);
 }
+
+//Get signle product by it's slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+}
